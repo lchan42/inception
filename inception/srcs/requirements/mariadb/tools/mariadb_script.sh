fdbfae31c6e439ff1d64cff443d6a89a41db6eca
Luc CHAN <lchan@e3r5p8.clusters.42paris.fr>
2023-03-13 13:22:49 +0100

    mariadb done ?

--- inception/srcs/requirements/mariadb/tools/mariadb_script.sh
+++ inception/srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -1,11 +1,14 @@
 echo "hello from MAria"
 
-servce mysql start;
+service mysql start;
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PWD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PWD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PWD}';"
 mysql -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p$MYSQL_ROOT_PWD shutdown
-exec mysqld_safe
 
+
+echo "goodbye from MAria"
+
+exec mysqld_safe
