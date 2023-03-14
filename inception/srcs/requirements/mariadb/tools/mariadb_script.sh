bcb9b45ff838d3cfeadead33e36d8c3ba993ddd7
Luc CHAN <lchan@e1r2p2.clusters.42paris.fr>
2023-03-14 15:14:14 +0100

    mariadb you bitc*

--- inception/srcs/requirements/mariadb/tools/mariadb_script.sh
+++ inception/srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -1,38 +1,59 @@
-
-
-echo "hello from MAria"
-
 export MYSQL_DATABASE=Wordpress
 export MYSQL_ROOT_PWD=MariadbRootPwd
 export MYSQL_USER=MariadbUser
 export MYSQL_PWD=MariadbPwd
 
 
-
-
 service mysql start;
-mysql -e " --user=root mysql"
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PWD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PWD}';"
-echo "hello 5"
 
-mysql -e "UPDATE mysql.user SET Password='${MYSQL_ROOT_PWD}' WHERE User='root';"
-echo "hello 6"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PWD}';"
-mysql -e "FLUSH PRIVILEGES;"
-echo "hello 7"
-mysql -e "SHOW GRANTS FOR 'root'@'%';"
-mysql -e "SHOW GRANTS FOR '${MYSQL_USER}'@localhost;"
-echo "hello 8"
+mysql -u root -p${MYSQL_ROOT_PWD} -e "UPDATE mysql.user  SET Password='${MYSQL_ROOT_PWD}' WHERE User='root';"
+mysql -u root -p${MYSQL_ROOT_PWD} -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PWD}';"
+mysql -u root -p${MYSQL_ROOT_PWD} -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+mysql -u root -p${MYSQL_ROOT_PWD} -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PWD}';"
+mysql -u root -p${MYSQL_ROOT_PWD} -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PWD}';"
+mysql -u root -p${MYSQL_ROOT_PWD} -e "SELECT user, host, password, Shutdown_priv FROM mysql.user;"
+mysql -u root -p${MYSQL_ROOT_PWD} -e "FLUSH PRIVILEGES;"
+killall -15 mysqld
+# mysqladmin --user=root -p=${MYSQL_ROOT_PWD} shutdown
+echo couocou
+sleep 5 && exec mysqld_safe
 
-echo "hello 9"
-echo "goodbye from Maria"
+# && mysql -e "SHOW GRANTS FOR 'root'@'%';"\
+# && mysql -e "SHOW GRANTS FOR 'root'@'localhost';"\
+# && mysql -e "SHOW GRANTS FOR '${MYSQL_USER}'@localhost;"\
+
+
+
+# mysqladmin -u root -p${MYSQL_ROOT_PWD} shutdown;
+# mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '';"
+
+# mysql -e "mysql -u root -p${MYSQL_ROOT_PWD};"
+
+# killall -15 mysqld
+
+# echo "goodbye from Maria"
+
+# exec mysqld_safe -p${MYSQL_ROOT_PWD}
+
+# 'root'@'localhost' root accession the DB from localhost.
+# 'root'@'%' root accession the DB from any host.
+
+
+
+
+# #### just a reminder ####
+# # mysql -u root -p --> --updade --password
+# mysql -e "Select user from mysql.user;" --> show all user
+# mysql -e "DESC mysql.user;" -->
+# mysql -e "SELECT user, host, password, Shutdown_priv FROM mysql.user;"
+
+
+
+
 
 
 # mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PWD}';"
 # mysql -e "FLUSH PRIVILEGES;"
-# killall -15 mysqld
 # mysqladmin -u root -p$MYSQL_ROOT_PWD shutdown
 # service mysql -u -root -p start ;
 # systemctl start mysqld;
@@ -42,33 +63,3 @@ echo "goodbye from Maria"
 
 #### just a reminder ####
 # mysql -u root -p --> --updade --password
-
-
-# service mysql start;
-# echo "hello 1"
-# mysql -e " --user=root mysql"
-# echo "hello 2"
-# mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-
-# echo "hello 3"
-# mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PWD}';"
-# echo "hello 4"
-# mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PWD}';"
-# echo "hello 5"
-# mysql -e "UPDATE mysql.user SET Password=${MYSQL_ROOT_PWD} WHERE User='root';"
-# echo "hello 6"
-# mysql -e "FLUSH PRIVILEGES;"
-# echo "hello 7"
-# # mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PWD}';"
-# # mysql -e "FLUSH PRIVILEGES;"
-# # killall -15 mysqld
-# mysqladmin -u root -p $MYSQL_ROOT_PWD shutdown
-# echo "hello 8"
-# # service mysql -u -root -p start ;
-# # systemctl start mysqld;
-# exec mysqld_safe
-# echo "hello 9"
-# echo "goodbye"
-
-# #### just a reminder ####
-# # mysql -u root -p --> --updade --password
