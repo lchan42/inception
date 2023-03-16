cccff1d3c14e2592b580128fa52228a2f39c4f1c
Luc CHAN <lchan@e1r11p5.clusters.42paris.fr>
2023-03-16 12:14:17 +0100

    mariadb ??

--- inception/srcs/requirements/mariadb/tools/mariadb_script.sh
+++ inception/srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -3,20 +3,60 @@ export MYSQL_ROOT_PWD=MariadbRootPwd
 export MYSQL_USER=MariadbUser
 export MYSQL_PWD=MariadbPwd
 
+set -x
 
-service mysql start;
+# mysql_install_db
 
-mysql -u root -p${MYSQL_ROOT_PWD} -e "UPDATE mysql.user  SET Password='${MYSQL_ROOT_PWD}' WHERE User='root';"
-mysql -u root -p${MYSQL_ROOT_PWD} -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PWD}';"
-mysql -u root -p${MYSQL_ROOT_PWD} -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-mysql -u root -p${MYSQL_ROOT_PWD} -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PWD}';"
-mysql -u root -p${MYSQL_ROOT_PWD} -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PWD}';"
-mysql -u root -p${MYSQL_ROOT_PWD} -e "SELECT user, host, password, Shutdown_priv FROM mysql.user;"
-mysql -u root -p${MYSQL_ROOT_PWD} -e "FLUSH PRIVILEGES;"
-killall -15 mysqld
-# mysqladmin --user=root -p=${MYSQL_ROOT_PWD} shutdown
-echo couocou
-sleep 5 && exec mysqld_safe
+service mysql start
+
+sleep 2
+
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
+then
+
+	echo "Database already exists"
+else
+
+# Set root option so that connexion without root password is not possible
+
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PWD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PWD}';"
+mysql -e "UPDATE mysql.user  SET Password='${MYSQL_ROOT_PWD}' WHERE User='root';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PWD}';"
+mysql -e "SELECT user, host, password, Shutdown_priv FROM mysql.user;"
+mysql -e "FLUSH PRIVILEGES;"
+fi
+
+sleep 2
+
+sudo service mysql restart
+
+
+# while ! mysqladmin ping -h mariadb --silent; do
+# 	sleep 1
+# done
+
+set +x
+
+
+# mysql -e "UPDATE mysql.user  SET Password='${MYSQL_ROOT_PWD}' WHERE User='root';"
+# mysql -uroot -p${MYSQL_ROOT_PWD} -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PWD}';"
+# mysql -uroot -p${MYSQL_ROOT_PWD} -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+# mysql -uroot -p${MYSQL_ROOT_PWD} -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PWD}';"
+# mysql -uroot -p${MYSQL_ROOT_PWD} -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PWD}';"
+# mysql -uroot -p${MYSQL_ROOT_PWD} -e "SELECT user, host, password, Shutdown_priv FROM mysql.user;"
+# mysql -uroot -p${MYSQL_ROOT_PWD} -e "FLUSH PRIVILEGES;"
+# mysql -uroot -p${MYSQL_ROOT_PWD} -e "QUIT"
+# mysql -uroot -p${MYSQL_ROOT_PWD} -e "SHOW GRANTS FOR 'root'@'localhost';"
+
+# echo "UPDATE mysql.user SET Password='${MYSQL_ROOT_PWD}' WHERE User='root'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PWD}'; CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`; CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PWD}'; GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PWD}'; SELECT user, host, password, Shutdown_priv FROM mysql.user; FLUSH PRIVILEGES; QUIT;" | mysql -u root
+
+# killall -15 mysqld
+# service mysql stop;
+# mysqladmin -u root -p${MYSQL_ROOT_PWD} shutdown
+# wait;
+# exec mysqld_safe --skip-grant-tables
 
 # && mysql -e "SHOW GRANTS FOR 'root'@'%';"\
 # && mysql -e "SHOW GRANTS FOR 'root'@'localhost';"\
