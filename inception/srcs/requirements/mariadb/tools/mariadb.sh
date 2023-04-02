e8da187a9c1626a80fa9078e9d6bc4a99196cf49
Luc Chan <luc92.chan@gmail.com>
2023-04-02 13:53:06 +0200

    new user, wordpress + mariadb rootopt

--- inception/srcs/requirements/mariadb/tools/mariadb.sh
+++ inception/srcs/requirements/mariadb/tools/mariadb.sh
@@ -5,7 +5,7 @@
 
 # set -x
 
-
+# rootopt="-u root -p${MYSQL_ROOT_PWD} -e"
 
 if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
 then
@@ -15,12 +15,20 @@ else
 service mysql start
 sleep 4
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PWD}';"
-mysql -u root -p${MYSQL_ROOT_PWD} -e  "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PWD}';"
+mysql -u root -p${MYSQL_ROOT_PWD} -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PWD}';"
 mysql -u root -p${MYSQL_ROOT_PWD} -e "FLUSH PRIVILEGES;"
 mysql -u root -p${MYSQL_ROOT_PWD} -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 mysql -u root -p${MYSQL_ROOT_PWD} -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PWD}';"
 mysql -u root -p${MYSQL_ROOT_PWD} -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PWD}';"
 mysql -u root -p${MYSQL_ROOT_PWD} -e "FLUSH PRIVILEGES;"
+
+# mysql $rootopt "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PWD}';"
+# mysql $rootopt "FLUSH PRIVILEGES;"
+# mysql $rootopt "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+# mysql $rootopt "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PWD}';"
+# mysql $rootopt "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PWD}';"
+# mysql $rootopt "FLUSH PRIVILEGES;"
+
 mysqladmin -u root -p${MYSQL_ROOT_PWD} shutdown
 sleep 2
 fi
--- inception/srcs/requirements/wordpress/conf/auto_config.sh
+++ inception/srcs/requirements/wordpress/conf/auto_config.sh
@@ -2,31 +2,38 @@
 
 
 sleep 10
-wp core download --allow-root --path='/var/www/wordpress'
-echo "wordpress downloaded"
-wp config create	--allow-root \
-					--dbname=$MYSQL_DATABASE \
-					--dbuser=$MYSQL_USER \
-					--dbpass=$MYSQL_PWD \
-					--dbhost=mariadb:3306 \
-					--path='/var/www/wordpress'
-echo "wp-config.php created"
+#download wordpress
+wp core download			--allow-root --path='/var/www/wordpress'
 
-wp core install		--allow-root --locale=fr_FR --skip-email \
-					--url='https://lchan.42.fr' \
-					--title=$WORDPRESS_TITLE \
-					--admin_user=$WORDPRESS_ADMIN_USER \
-					--admin_password=$WORDPRESS_ADMIN_PASSWORD \
-					--admin_email=$WORDPRESS_ADMIN_EMAIL \
-					--path='/var/www/wordpress'
-echo "Wordpress installed"
+# creat wp-config.php
+wp config create			--allow-root \
+							--dbname=$MYSQL_DATABASE \
+							--dbuser=$MYSQL_USER \
+							--dbpass=$MYSQL_PWD \
+							--dbhost=mariadb:3306 \
+							--path='/var/www/wordpress'
 
-#wp user create 	--allow-root ${WORDPRESS_USER_2} ${WORDPRESS_USER_2_EMAIL}
---role=author
---user_pass=${WORDPRESS_USER_2_MDP}
---path='/var/www/wordpress'
-# wp cache flush --allow-root
-# wp theme install wpkites --activate --allow-root
+# install wordpress
+# test with skip-email
+wp core install				--allow-root --locale=fr_FR --skip-email \
+							--url='https://lchan.42.fr' \
+							--title=$WORDPRESS_TITLE \
+							--admin_user=$WORDPRESS_ADMIN_USER \
+							--admin_password=$WORDPRESS_ADMIN_PASSWORD \
+							--admin_email=$WORDPRESS_ADMIN_EMAIL \
+							--path='/var/www/wordpress'
+
+# creat new user
+wp user create 				--allow-root ${WORDPRESS_USER_2} ${WORDPRESS_USER_2_EMAIL} \
+							--role=author \
+							--user_pass=${WORDPRESS_USER_2_MDP} \
+							--path='/var/www/wordpress'
+
+# update privilages
+wp cache flush				--allow-root
+
+# installing a theme
+wp theme install wpkites	--activate --allow-root
 
 
 mkdir /run/php
@@ -34,3 +41,5 @@ mkdir /run/php
 # -F : Force to stay in foreground and ignore daemonize option from configuration file.
 exec /usr/sbin/php-fpm7.3 -F -R
 
+# note :
+# [--role=<role>] Possible values include ‘administrator’, ‘editor’, ‘author’, ‘contributor’, ‘subscriber’.
